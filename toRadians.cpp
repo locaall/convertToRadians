e1c282c7da34e52852406ac8284bfd85f0ad21e2
RaleEZ <81446305+RaleEZ@users.noreply.github.com>
2022-12-29 17:10:20 +0100

    Update and rename Radijani_u_Stepene.cpp to toRadians.cpp

--- toRadians.cpp
+++ toRadians.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 
-const double PI = 4*atan(1);
-int x, y;
-char a;
+const double PI = 4*atan(1); // Making a constant PI variable
+int x, y; // Integer x represents amount of degrees and y number of decimals
+char a; // Simple char for 'Yes' and 'No'
 
 int main() {
-    std::cout << "Unesite vrednost ugla za konvertovanje u radijane: "; std::cin >> x;
-    double radijan = x * PI/180;
-    std::cout << "Ugao koji ste uneli u radijanima: " << radijan << std::endl; 
-    std::cout << "---------------------------------" << std::endl;
-    std::cout << "Da li zelite da zaokruglim ovaj broj na X broj decimala? "; std::cin >> a;
+    std::cout << "Unesite vrednost ugla za konvertovanje u radijane: "; std::cin >> x; // "Input the value of angle: "
+    double radijan = x * PI/180; // initializing radian
+    std::cout << "Ugao koji ste uneli u radijanima: " << radijan << std::endl; // "Number you entered: "
+    std::cout << "---------------------------------" << std::endl; 
+    std::cout << "Da li zelite da zaokruglim ovaj broj na X broj decimala? "; std::cin >> a; // "Do you want me to round this number?"
 
-    if (a == 'Y' || a == 'y') {
-        std::cout << "Vas odgovor je 'DA'. Molimo Vas da unesete broj (1-5): "; std::cin >> y;
+    if (a == 'Y' || a == 'y') { // Simple input
+        std::cout << "Vas odgovor je 'DA'. Molimo Vas da unesete broj (1-5): "; std::cin >> y; // "Your answer is 'YES'. Please enter a number (1-5): "
 
-        switch (y) {
+        switch (y) { // All those cout's say "I am rounding the number to y decimals", and then they print the rounded number 
         case 1:
             std::cout << "Uneli ste broj " << y << ". Zaokrugljujem broj na " << y << " decimalu.\nVas broj zaokrugljen na " << y << "-u decimalu: " << std::fixed << std::showpoint << std::setprecision(y) << radijan << std::endl;
             break;
@@ -32,10 +32,7 @@ int main() {
             break;
     }   
     } else if (a == 'N' || a == 'n') {
-        std::cout << "Vas odgovor je 'NE'. Vas broj bez zaokrugljivanja iznosi: " << radijan << std::endl;
+        std::cout << "Vas odgovor je 'NE'. Vas broj bez zaokrugljivanja iznosi: " << radijan << std::endl; // "Your answer is 'NO'. Your number of radians without rounding: "
     }
-
-    
-
     return 0;
 }
